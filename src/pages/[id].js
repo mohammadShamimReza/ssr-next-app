d87be3837e999015de6f2b1e7fa646bb6dd450df
GitMohammad <mohammadshamimreza23393@gmail.com>
2023-01-31 19:23:50 +0600

    fixed dynamic route

---
+++ src/pages/[id].js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+export async function getStaticPaths() {
+  console.log(id);
+  const res = await fetch(
+    `https://www.initmigrate-spz457y-uinpbvoggscfo.us-4.platformsh.site/api/basic-pages?populate=*&pagination[limit]=50`
+  );
+  const data = await res.json();
+  const paths = data.data.map((enity) => {
+    return {
+      params: { id: enity.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false, // will be passed to the page component as props
+  };
+}
+export async function getStaticProps(context) {
+  const res = await fetch(
+    `https://www.initmigrate-spz457y-uinpbvoggscfo.us-4.platformsh.site/api/basic-pages/${context.params.id}/?populate=*`
+  );
+  const data = await res.json();
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
+
+function id({ data }) {
+  console.log(data);
+  return (
+    <div>
+      <h1>Entity id is </h1>
+    </div>
+  );
+}
+
+export default id;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import styles from "@/styles/Home.module.css";
 import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
 export async function getServerSideProps(context) {
   const res = await fetch(
     "https://www.initmigrate-spz457y-uinpbvoggscfo.us-4.platformsh.site/api/basic-pages?populate=*&pagination[limit]=50"
@@ -12,21 +14,36 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home({ data }) {
-  console.log(data.data);
+  const [first, setfirst] = useState("");
+
+  console.log(first);
 
   return (
     <>
       <main>
         <Head className={styles.center}>
-          <h1>All Basic Page entitys</h1>
+          {/* <h1>All Basic Page entitys </h1> */}
         </Head>
         {data?.data?.length === 0 ? (
           <div className="">LOading</div>
         ) : (
           data?.data?.map((entity) => (
-            <div key={entity.id} className={styles.description}>
-              <p>{entity?.attributes.title}</p>
-            </div>
+            <Link
+              href={"/" + entity.id}
+              onClick={() => console.log(entity.id)}
+              key={entity.id}
+              className={styles.description}
+            >
+              <br />
+              <p style={{ cursor: "pointer" }}>
+                <button
+                  style={{ cursor: "pointer", border: "none" }}
+                  // style={{}}
+                >
+                  <h3>{entity?.attributes.title}</h3>
+                </button>{" "}
+              </p>
+            </Link>
           ))
         )}
         {}
